5b20b9c27ca1bcf95f48ac4f8c99e56f71d360ed
Juan Sebastián Martin <38918218+sebamartin10@users.noreply.github.com>
2021-06-16 21:28:06 -0300

    Added PlayerController

---
+++ APITopicTwister/Controllers/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using APITopicTwister.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITopicTwister.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public Player CreatePlayer() {
+            return null;
+        }
+        
+    }
+}
