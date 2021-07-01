8db6b39b6e15735a13fae313e551982b4a5d6604
noemimorales <79290160+noemimorales@users.noreply.github.com>
2021-07-01 15:24:41 -0300

    wordCategory service and controller

---
+++ Repository/Contracts/IWordCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Contracts
+{
+    public interface IWordCategoryRepository
+    {
+        public void Create(WordCategory wordCategory);
+        public void Delete(WordCategory wordCategory);
+        public WordCategory FindByWordCategoryID(string WordCategoryID);
+        public List<WordCategory> FindAllWordCategory();
+    }
+}
---
+++ Services/DTOs/WordCategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs
+{
+    public class WordCategoryDTO
+    {
+        public string WordCategoryID { get; set; }
+        public string CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        public string WordID { get; set; }
+        public virtual Word Word { get; set; }
+
+    }
+}
