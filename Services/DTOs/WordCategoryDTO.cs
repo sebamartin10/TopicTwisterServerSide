dd43e8a28e783095558e966217ac222357fa822b
noemimorales <79290160+noemimorales@users.noreply.github.com>
2021-07-01 15:50:27 -0300

    update wordcategoryDTO

--- Services/DTOs/WordCategoryDTO.cs
+++ Services/DTOs/WordCategoryDTO.cs
@@ -11,9 +11,7 @@ namespace Services.DTOs
     {
         public string WordCategoryID { get; set; }
         public string CategoryID { get; set; }
-        public virtual Category Category { get; set; }
         public string WordID { get; set; }
-        public virtual Word Word { get; set; }
 
     }
 }
