5dfc80d9e10c4c57e0fe7adc775c99436db14eb4
noemimorales <79290160+noemimorales@users.noreply.github.com>
2021-07-17 15:15:50 -0300

    round result

---
+++ Services/DTOs/RoundResultDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs
+{
+    public class RoundResultDTO
+    {
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+        public virtual List<RoundResultByCategoryDTO> RoundResults { get; set; }
+        public char Letter { get; set; }
+        public bool isPlayer1Winner { get; set; }
+        public bool isPlayer2Winner { get; set; }
+    }
+}
