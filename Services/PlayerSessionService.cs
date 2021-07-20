88d9a0c6629c3419929211c2544ffbc262633e4b
Juan Pablo Artola <juanpabloartola@gmail.com>
2021-07-20 19:36:52 -0300

    completed session dto on get session

--- Services/PlayerSessionService.cs
+++ Services/PlayerSessionService.cs
@@ -17,8 +17,7 @@ namespace Services
         IPlayerSessionRepository playerSessionRepository;
         public ResponseTopicTwister<List<FinishedSessionDTO>> GetFinishedSessionsByPlayer(string playerID) {
             ResponseTopicTwister<List<FinishedSessionDTO>> response = new ResponseTopicTwister<List<FinishedSessionDTO>>();
-            try
-            {
+            try {
                 RoundRepository roundRepo = new RoundRepository();
                 TurnRepository turnRepo = new TurnRepository();
                 SessionService sessionService = new SessionService();
@@ -26,8 +25,7 @@ namespace Services
                 response.Dto = new List<FinishedSessionDTO>();
                 PlayerSessionRepository playerSessionRepo = new PlayerSessionRepository();
                 List<Session> finishedSessions = playerSessionRepo.FindAllNoActivePlayerSessions(playerID);
-                if (finishedSessions.Count == 0)
-                {
+                if (finishedSessions.Count == 0) {
 
                     response.ResponseMessage = "No se pudo recuperar un historial de sesiones para el player";
                     return response;
@@ -36,7 +34,7 @@ namespace Services
                 foreach (var sessionFinished in finishedSessions) {
                     FinishedSessionDTO finishedSessionDTO = new FinishedSessionDTO();
                     List<SessionResult> sessionsResults = sessionService.GetSessionResults(sessionFinished.SessionID);
-                    
+
                     finishedSessionDTO.playerLocalStatus = sessionsResults.Where(x => x.Player.PlayerID == playerID).First().StatusPlayer;
                     Player playerOponent = sessionsResults.Where(x => x.Player.PlayerID != playerID).First().Player;
                     finishedSessionDTO.OpponentID = playerOponent.PlayerID;
@@ -45,71 +43,56 @@ namespace Services
                 }
                 return response;
 
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 response.ResponseCode = -1;
                 response.ResponseMessage = ex.Message;
                 return response;
             }
 
-       
+
         }
-        public ResponseTopicTwister<List<ActiveSessionDTO>> GetActiveSessionsByPlayer(string playerID)
-        {
+        public ResponseTopicTwister<List<ActiveSessionDTO>> GetActiveSessionsByPlayer(string playerID) {
             ResponseTopicTwister<List<ActiveSessionDTO>> response = new ResponseTopicTwister<List<ActiveSessionDTO>>();
-            try
-            {
+            try {
                 RoundRepository roundRepo = new RoundRepository();
                 TurnRepository turnRepo = new TurnRepository();
-                
+
                 response.Dto = new List<ActiveSessionDTO>();
                 PlayerSessionRepository playerSessionRepo = new PlayerSessionRepository();
                 List<Session> activeSessions = playerSessionRepo.FindAllActivePlayerSessions(playerID);
-                if (activeSessions.Count == 0)
-                {
+                if (activeSessions.Count == 0) {
 
                     response.ResponseMessage = "No existen sesiones activas para el player";
                     return response;
                 }
 
-                foreach (var sessionActive in activeSessions)
-                {
+                foreach (var sessionActive in activeSessions) {
                     ActiveSessionDTO asDTO = new ActiveSessionDTO();
                     asDTO.SessionID = sessionActive.SessionID;
                     List<Round> rounds = roundRepo.FindBySession(sessionActive.SessionID);
                     asDTO.RoundsCount = rounds.Count;
 
-                    Round actualRound = rounds.Where(x => x.Finished == false).OrderBy(x=>x.roundNumber).First();
+                    Round actualRound = rounds.Where(x => x.Finished == false).OrderBy(x => x.roundNumber).First();
                     asDTO.CurrentRoundNumber = actualRound.roundNumber;
                     Player oponent = turnRepo.FindOponent(playerID);
                     asDTO.OpponentName = oponent.PlayerName;
-                    Turn actualTurn = actualRound.Turns.Where(x => x.finished == false && x.PlayerID == playerID).FirstOrDefault();
-                    if (actualTurn == null)
-                    {
-                        asDTO.IsLocalPlayer = true;
-                    }
-                    else
-                    {
-                        asDTO.IsLocalPlayer = false;
-                    }
+
+                    Turn actualTurn = actualRound.Turns.Where(x => x.finished == false).OrderBy(x => x.turnNumber).FirstOrDefault();
+                    asDTO.IsLocalPlayer = actualTurn != null && actualTurn.PlayerID == playerID;
+
                     response.Dto.Add(asDTO);
                 }
 
                 return response;
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 response.ResponseCode = -1;
                 response.ResponseMessage = ex.Message;
                 return response;
             }
-            
+
         }
-        public ResponseTopicTwister<PlayerSessionDTO> CreatePlayerSession(string playerID, string sessionID)
-        {
-            try
-            {
+        public ResponseTopicTwister<PlayerSessionDTO> CreatePlayerSession(string playerID, string sessionID) {
+            try {
                 ResponseTopicTwister<PlayerSessionDTO> response = new ResponseTopicTwister<PlayerSessionDTO>();
 
                 playerSessionRepository = new PlayerSessionRepository();
@@ -122,8 +105,7 @@ namespace Services
                 SessionRepository sessionRepository = new SessionRepository();
                 session = sessionRepository.FindById(sessionID);
 
-                PlayerSession playerSession = new PlayerSession()
-                {
+                PlayerSession playerSession = new PlayerSession() {
                     PlayerSessionID = Guid.NewGuid().ToString(),
                     PlayerID = player.PlayerID,
                     SessionID = session.SessionID
@@ -131,17 +113,13 @@ namespace Services
 
                 playerSessionRepository.Create(playerSession);
 
-                response.Dto = new PlayerSessionDTO
-
-                {
+                response.Dto = new PlayerSessionDTO {
                     PlayerSessionID = Guid.NewGuid().ToString(),
                     PlayerID = player.PlayerID,
                     SessionID = session.SessionID
                 };
                 return response;
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 return new ResponseTopicTwister<PlayerSessionDTO>(null, -1, ex.Message);
             }
         }
