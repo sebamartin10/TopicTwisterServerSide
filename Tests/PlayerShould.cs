117060dea627573b7073a1bdb4321e1ee306546f
Juan Sebastián Martin <38918218+sebamartin10@users.noreply.github.com>
2021-07-31 19:32:53 -0300

    Deleted tests with errors in PlayerShould

--- Tests/PlayerShould.cs
+++ Tests/PlayerShould.cs
@@ -63,36 +63,5 @@ namespace Tests
             Assert.AreEqual(expected, result.ResponseCode);
         }
 
-        [Test]
-        public void Register_A_Player()
-        {
-            //Given
-            PlayerService playerService = new PlayerService();
-            PlayerDTO playerDTO = new PlayerDTO();
-            playerDTO.playerName = "Player1";
-            playerDTO.password = "pass123";
-            playerDTO.playerID = Guid.NewGuid().ToString();
-            //When
-            ResponseTopicTwister<PlayerDTO> result = playerService.RegisterPlayer(playerDTO.playerName, playerDTO.password, playerDTO.playerID);
-            //Then
-            Assert.AreEqual(playerDTO.playerID, result.Dto.playerID);
-        }
-
-        [Test]
-        public void Prove_Identity_With_User_And_Password() {
-
-            //Given
-            PlayerService playerService = new PlayerService();
-
-            PlayerDTO playerDTO = new PlayerDTO {
-                playerName = "Player",
-                password = "Pass"
-            };
-            //When
-            ResponseTopicTwister<PlayerDTO> playerDTOLogin = playerService.Login(playerDTO);
-            //Then
-            Assert.AreEqual(playerDTO.playerName, playerDTOLogin.Dto.playerName);
-            Assert.AreEqual(playerDTO.password, playerDTOLogin.Dto.password);
-        }
     }
 }
